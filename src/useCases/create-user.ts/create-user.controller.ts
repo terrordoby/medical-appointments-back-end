d50ddf2705b76b100b5d2760ab0d93ca22177a8f
Gabriel Neto <gabriel.jorge@softwell.com.br>
2023-03-26 19:27:58 -0300

    Instalação e configuração do winston para logs

--- src/useCases/create-user.ts/create-user.controller.ts
+++ src/useCases/create-user.ts/create-user.controller.ts
@@ -1,15 +1,19 @@
 import {Request, Response} from 'express'
 import { CreateUserCase } from './create-user.usercase'
+import { logger } from '../../utils/logger';
 
 export class CreateUserController {
+    
   async handle(request: Request, response: Response) {
+    logger.info("Usuário sendo criado")
     try {
       const data = request.body;
       const user = await new CreateUserCase().execute(data);
 
     return response.json(user)
-    } catch (err) {
-        return response.status(400).json(err)
+    } catch (err: any) {
+        logger.error(err.stack)
+        return response.status(400).json(err.message)
     }
   }
 }
